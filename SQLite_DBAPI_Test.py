2c95f4e280cb04521f04489318902a12b42cf592
Eating <s15115148@stu.edu.tw>
2018-01-28 23:42:16 +0800

    Recieve data from CC2530_UART

--- SQLite_DBAPI_Test.py
+++ SQLite_DBAPI_Test.py
@@ -10,22 +10,23 @@ print "Opened database successfully";
 c = conn.cursor()
 '''
 name = 'Eating'
-c.execute("SELECT * FROM COMPANY WHERE NAME= '%s' " % name);
+c.execute("SELECT * FROM COMANY WHERE NAME= '%s' " % name);
 '''
 
 t = ('Paul',)
-c.execute('SELECT * FROM COMPANY WHERE NAME=?', t)
+c.execute('SELECT * FROM COMANY WHERE NAME=?', t)
 print c.fetchone
 
 data = [(2,'Eating',21,'Taiwan',50000),
-        (3,'Yi-Ting',15,'UK',100000)]
+        (3,'Yi-Ting',15,'UK',100000),
+        (4,'QQ',56,'USA',20000)]
 
-c.executemany('INSERT INTO COMPANY VALUES(?,?,?,?,?)', data)
+c.executemany('INSERT INTO COMANY VALUES(?,?,?,?,?)', data)
 
 conn.commit()
 print "Records created successfully";
 
-for row in c.execute('SELECT * FROM COMPANY'):
+for row in c.execute('SELECT * FROM COMANY'):
     print row
     time.sleep(1)
     
