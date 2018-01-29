fc4c9c36e7ebfef96ef178478e91c4edbfe42a3a
FullofQ <s15115148@stu.edu.tw>
2018-01-29 23:54:38 +0800

    UART_Datatype_Error--author=Eating <s15115148@stu.edu.tw>

--- Test/UART.py
+++ Test/UART.py
@@ -2,7 +2,7 @@
 
 import serial
 import time
- 
+
 ser = serial.Serial(
     port='/dev/serial0',
     baudrate = 115200,
@@ -15,8 +15,10 @@ counter = 0
  
 while 1:
     x=ser.read(100)
-    print x
-    time.sleep(1)
+    #print x
+    time.sleep(0.5)
+    alarm = __builtins__.str(x)
+    print alarm
     if x == '':
         print('Nothing')
     else:
