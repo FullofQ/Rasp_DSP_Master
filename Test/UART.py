41fb100507db7c699ba3a60cb1d2260c9e43459d
FullofQ <s15115148@stu.edu.tw>
2018-01-30 18:07:34 +0800

    Python_Serial_Datatype_I--author=Eating <s15115148@stu.edu.tw>

--- Test/UART.py
+++ Test/UART.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+__builtins__
+
 import serial
 import time
 
@@ -14,12 +16,13 @@ ser = serial.Serial(
 counter = 0
  
 while 1:
-    x=ser.read(100)
-    #print x
-    time.sleep(0.5)
-    alarm = __builtins__.str(x)
-    print alarm
-    if x == '':
+    x = ser.read(2)
+    #str_x = __builtins__.str(x)
+    x_hex = x.encode("hex")
+    print x_hex
+    #int_x = __builtins__.int(str_x)
+    time.sleep(1)
+    if x_hex == '':
         print('Nothing')
     else:
         print('Got it')
